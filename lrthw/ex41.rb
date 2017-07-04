62ffb247d41f427e9e21d438ab75fa63d6bba0e4
Carol Schmitz <carolgrrr@gmail.com>
2017-07-04 18:52:36 -0500

    ex 41 start

---
+++ lrthw/ex41.rb
@@ -0,0 +1,10 @@
+require 'open-uri'
+
+WORD_URL = "http://learncodethehardway.org/words.txt"
+WORDS = []
+
+PHRASES = {
+	"class ### < ###\nend" =>
+		"Make a class named ### that is-a ###.",
+	"class ###\n\tdef initialize(@@@)\n\tend\nend" =>
+}
